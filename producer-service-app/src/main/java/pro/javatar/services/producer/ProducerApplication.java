8d6f93ec9ca75b723c104f45c68c03a96f8517d5
Serhii Petrychenko <serhii.petrychenko@javatar.pro>
2018-05-27 22:20:07 +0300

    removed unnecessary annotation

--- producer-service-app/src/main/java/pro/javatar/services/producer/ProducerApplication.java
+++ producer-service-app/src/main/java/pro/javatar/services/producer/ProducerApplication.java
@@ -2,10 +2,8 @@ package pro.javatar.services.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
 public class ProducerApplication {
