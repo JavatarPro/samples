206bcf4aa40ae93155033512b09eb957c3dba37a
Serhii Petrychenko <spetrychenko@javatar.pro>
2018-11-30 00:37:10 +0100

    added copyright

--- user-service/user-rest-server/src/test/java/pro/javatar/core/converter/UserTOConverterTest.java
+++ user-service/user-rest-server/src/test/java/pro/javatar/core/converter/UserTOConverterTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 Javatar LLC
+ * All rights reserved.
+ */
+
 package pro.javatar.core.converter;
 
 import org.junit.Before;
--- user-service/user-service-impl/src/test/java/pro/javatar/core/service/converter/UserBOConverterTest.java
+++ user-service/user-service-impl/src/test/java/pro/javatar/core/service/converter/UserBOConverterTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 Javatar LLC
+ * All rights reserved.
+ */
+
 package pro.javatar.core.service.converter;
 
 import org.junit.Before;
